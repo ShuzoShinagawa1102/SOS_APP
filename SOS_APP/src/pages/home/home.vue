78b122edc06412fec193f7fc7a284bc6077d2922
Shuzo <cssz20040@g.nihon-u.ac.jp>
2023-09-24 01:27:18 +0900

    first commit

---
+++ SOS_APP/src/pages/home/home.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<web-view :webview-styles="webviewStyles" src="/hybrid/html/home.html"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				webviewStyles: {
+					progress: {
+						color: '#FF3333'
+					}
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
